8a42945f8e0d8b55d94c18a4f675100653b6052a
Kurt Raschke <kurt@kurtraschke.com>
2013-09-22 17:33:13 -0400

    Switch to Jetty WebSocket client.
    Because we no longer need compatability with Java 1.6, we can switch to the Jetty WebSocket client.

--- onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/GtfsRealtimeFeedImpl.java
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/GtfsRealtimeFeedImpl.java
@@ -29,21 +29,26 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import org.eclipse.jetty.websocket.api.CloseStatus;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.onebusaway.transit_data_federation.services.realtime.gtfs_realtime.GtfsRealtimeEntityListener;
 import org.onebusaway.transit_data_federation.services.realtime.gtfs_realtime.GtfsRealtimeFeed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.protobuf.ExtensionRegistry;
 import com.google.transit.realtime.GtfsRealtime.FeedEntity;
 import com.google.transit.realtime.GtfsRealtime.FeedHeader;
 import com.google.transit.realtime.GtfsRealtime.FeedMessage;
 import com.google.transit.realtime.GtfsRealtimeOneBusAway;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.websocket.DefaultWebSocketListener;
-import com.ning.http.client.websocket.WebSocket;
-import com.ning.http.client.websocket.WebSocketUpgradeHandler;
 
 /**
  *
@@ -56,7 +61,8 @@ public class GtfsRealtimeFeedImpl implements GtfsRealtimeFeed {
     private int _refreshInterval;
     private GtfsRealtimeEntityListener _entityListener;
     private boolean _isPush;
-    private GtfsRealtimeListener _listener;
+    private WebSocketClient _webSocketClient;
+    private GtfsRealtimeClientSocket _socket;
     private Map<String, FeedEntity> _feedEntityById = new ConcurrentHashMap<String, FeedEntity>();
     private ScheduledExecutorService _scheduledExecutorService;
     private ScheduledFuture<?> _refreshTask;
@@ -93,30 +99,41 @@ public class GtfsRealtimeFeedImpl implements GtfsRealtimeFeed {
             _refreshTask.cancel(true);
             _refreshTask = null;
         }
+
         try {
-            if (_listener != null) {
-                _listener.close();
-                _listener = null;
+            if (_socket != null) {
+                _socket.close();
+                _socket = null;
             }
         } catch (Exception e) {
-            _log.error("Exception stopping listener", e);
+            _log.error("Exception stopping WebSocket connection", e);
+        }
+
+        try {
+            if (_webSocketClient != null) {
+                _webSocketClient.stop();
+                _webSocketClient = null;
+            }
+        } catch (Exception e) {
+            _log.error("Exception stopping WebSocket client", e);
         }
     }
     
     private void startPush() {
-        AsyncHttpClient client = new AsyncHttpClient();
-        _listener = new GtfsRealtimeListener();
-        WebSocketUpgradeHandler handler = new WebSocketUpgradeHandler.Builder().addWebSocketListener(_listener).build();
+        _webSocketClient = new WebSocketClient();
+        _socket = new GtfsRealtimeClientSocket();
         try {
-            WebSocket socket = client.prepareGet(_endpoint.toString()).execute(handler).get();
-        } catch (Exception e) {
+            _webSocketClient.start();
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            _webSocketClient.connect(_socket, _endpoint, request);
+            _log.debug(String.format("Connecting to : %s%n", _endpoint));
+        } catch (Throwable t) {
             /*
              * If something went wrong (endpoint down, etc.), try to set up again
              * after _refreshInterval seconds.
              */
-            _log.error("Error starting WebSocket client", e);
+            _log.error("Error starting WebSocket client", t);
             _scheduledExecutorService.schedule(new RestartTask(), _refreshInterval, TimeUnit.SECONDS);
-            
         }
         
     }
@@ -198,32 +215,46 @@ public class GtfsRealtimeFeedImpl implements GtfsRealtimeFeed {
             startPush();
         }
     }
-    
-    private class GtfsRealtimeListener extends DefaultWebSocketListener {
-        
+
+    @WebSocket(maxMessageSize = 16384000, maxIdleTime = 300)
+    public class GtfsRealtimeClientSocket {
+
         private boolean _closeInProgress = false;
-        private WebSocket _socket;
-        
-        public GtfsRealtimeListener() {
+        private Session _session;
+
+        public GtfsRealtimeClientSocket() {
         }
-        
+
         public void close() throws IOException {
             _closeInProgress = true;
-            _socket.close();
+            _session.close(new CloseStatus(StatusCode.SHUTDOWN, "Client shutting down."));
         }
-        
-        @Override
-        public void onClose(WebSocket socket) {
+
+        @OnWebSocketError
+        public void onError(Throwable error) {
+            _log.error("Error streaming GTFS-realtime updates", error);
+
+        }
+
+        @OnWebSocketClose
+        public void onClose(int statusCode, String reason) {
+            _log.info(String.format("Connection closed: %d - %s%n", statusCode, reason));
+            _session = null;
             if (!_closeInProgress) {
                 _log.warn("Unexpected close; attempting restart");
                 _scheduledExecutorService.schedule(new RestartTask(), _refreshInterval, TimeUnit.SECONDS);
             }
         }
-        
-        @Override
-        public void onMessage(byte[] message) {
+
+        @OnWebSocketConnect
+        public void onConnect(Session session) {
+            _session = session;
+        }
+
+        @OnWebSocketMessage
+        public void onMessage(byte buf[], int offset, int length) {
             try {
-                InputStream stream = new ByteArrayInputStream(message);
+                InputStream stream = new ByteArrayInputStream(buf, offset, length);
                 FeedMessage fm = readFeedFromStream(stream);
                 handleFeedMessage(fm);
             } catch (Throwable t) {
@@ -231,14 +262,5 @@ public class GtfsRealtimeFeedImpl implements GtfsRealtimeFeed {
             }
         }
         
-        @Override
-        public void onOpen(WebSocket socket) {
-            _socket = socket;
-        }
-        
-        @Override
-        public void onError(Throwable t) {
-            _log.error("Error streaming GTFS-realtime updates", t);
-        }
     }
 }
